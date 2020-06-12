espressif/esp-idf

commit fbb54184ef006ae6644d0b52878414ec0b2af652

doc: add generation of tags from sdkconfig and x_caps.h headers
Also updates the way we handle exclude_patterns to use the new tags

Closes IDF-1484

docs/conf_common.py
@@ -67,6 +67,8 @@ extensions = ['breathe',
               'idf_extensions.gen_idf_tools_links',
               'idf_extensions.format_idf_target',
               'idf_extensions.latex_builder',
+              'idf_extensions.gen_defines',
+              'idf_extensions.exclude_docs',
 
               # from https://github.com/pfalcon/sphinx_selective_exclude
               'sphinx_selective_exclude.eager_only',
@@ -124,47 +126,49 @@ print('Version: {0}  Release: {1}'.format(version, release))
 exclude_patterns = ['**/inc/**', '_static', '**/_build']
 
 
-# Add target-specific excludes based on tags (for the IDF_TARGET). Haven't found any better way to do this yet
-def update_exclude_patterns(tags):
-    if "esp32" not in tags:
-        # Exclude ESP32-only document pages so they aren't found in the initial search for .rst files
-        # note: in toctrees, these also need to be marked with a :esp32: filter
-        for e in ['api-guides/blufi.rst',
-                  'api-guides/build-system-legacy.rst',
-                  'api-guides/esp-ble-mesh/**',
-                  'api-guides/RF_calibration.rst',  # temporary until support re-added in esp_wifi
-                  'api-guides/ulp-legacy.rst',
-                  'api-guides/unit-tests-legacy.rst',
-                  'api-guides/ulp_instruction_set.rst',
-                  'api-guides/jtag-debugging/configure-wrover.rst',
-                  'api-reference/system/himem.rst',
-                  'api-reference/bluetooth/**',
-                  'api-reference/peripherals/sdio_slave.rst',
-                  'api-reference/peripherals/esp_slave_protocol.rst',
-                  'api-reference/peripherals/mcpwm.rst',
-                  'api-reference/peripherals/sd_pullup_requirements.rst',
-                  'api-reference/peripherals/sdmmc_host.rst',
-                  'api-reference/protocols/esp_serial_slave_link.rst',
-                  'api-reference/system/ipc.rst',
-                  'get-started-legacy/**',
-                  'security/secure-boot-v1.rst',
-                  'security/secure-boot-v2.rst',
-                  'gnu-make-legacy.rst',
-                  'hw-reference/esp32/**',
-                  ]:
-            exclude_patterns.append(e)
+BT_DOCS = ['api-guides/blufi.rst',
+           'api-guides/esp-ble-mesh/**',
+           'api-reference/bluetooth/**']
 
-    if "esp32s2" not in tags:
-        # Exclude ESP32-S2-only document pages so they aren't found in the initial search for .rst files
-        # note: in toctrees, these also need to be marked with a :esp32: filter
-        for e in ['esp32s2.rst',
-                  'hw-reference/esp32s2/**',
-                  'api-guides/dfu.rst',
-                  'api-guides/ulps2_instruction_set.rst',
-                  'api-reference/peripherals/hmac.rst',
-                  'api-reference/peripherals/temp_sensor.rst']:
-            exclude_patterns.append(e)
+SDMMC_DOCS = ['api-reference/peripherals/sdmmc_host.rst',
+              'api-reference/peripherals/sd_pullup_requirements.rst']
 
+SDIO_SLAVE_DOCS = ['api-reference/peripherals/sdio_slave.rst',
+                   'api-reference/peripherals/esp_slave_protocol.rst',
+                   'api-reference/protocols/esp_serial_slave_link.rst']
+
+MCPWM_DOCS = ['api-reference/peripherals/mcpwm.rst']
+
+LEGACY_DOCS = ['api-guides/build-system-legacy.rst',
+               'gnu-make-legacy.rst',
+               'api-guides/ulp-legacy.rst',
+               'api-guides/unit-tests-legacy.rst',
+               'get-started-legacy/**']
+
+ESP32_DOCS = ['api-guides/ulp_instruction_set.rst',
+              'api-guides/jtag-debugging/configure-wrover.rst',
+              'api-reference/system/himem.rst',
+              'api-guides/RF_calibration.rst',
+              'api-reference/system/ipc.rst',
+              'security/secure-boot-v1.rst',
+              'security/secure-boot-v2.rst',
+              'hw-reference/esp32/**'] + LEGACY_DOCS
+
+ESP32S2_DOCS = ['esp32s2.rst',
+                'hw-reference/esp32s2/**',
+                'api-guides/ulps2_instruction_set.rst',
+                'api-guides/dfu.rst',
+                'api-reference/peripherals/hmac.rst',
+                'api-reference/peripherals/temp_sensor.rst'
+                '']
+
+# format: {tag needed to include: documents to included}, tags are parsed from sdkconfig and peripheral_caps.h headers
+conditional_include_dict = {'SOC_BT_SUPPORTED':BT_DOCS,
+                            'SOC_SDMMC_HOST_SUPPORTED':SDMMC_DOCS,
+                            'SOC_SDIO_SLAVE_SUPPORTED':SDIO_SLAVE_DOCS,
+                            'SOC_MCPWM_SUPPORTED':MCPWM_DOCS,
+                            'esp32':ESP32_DOCS,
+                            'esp32s2':ESP32S2_DOCS}
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
@@ -377,6 +381,8 @@ def setup(app):
     if "idf_target" not in app.config:
         app.add_config_value('idf_target', None, 'env')
         app.add_config_value('idf_targets', None, 'env')
+
+    app.add_config_value('conditional_include_dict', None, 'env')
 
     # Breathe extension variables (depend on build_dir)
     # note: we generate into xml_in and then copy_if_modified to xml dir

docs/idf_extensions/run_doxygen.py
@@ -2,7 +2,6 @@
 from __future__ import print_function
 from __future__ import unicode_literals
 from io import open
-import glob
 import os
 import os.path
 import re
@@ -45,27 +44,8 @@ def _parse_defines(header_path, sdk_config_path):
     return defines
 
 
-def generate_doxygen(app, project_description):
+def generate_doxygen(app, defines):
     build_dir = os.path.dirname(app.doctreedir.rstrip(os.sep))
-
-    sdk_config_path = os.path.join(project_description["build_dir"], "config")
-
-    # Parse kconfig macros to pass into doxygen
-    #
-    # TODO: this should use the set of "config which can't be changed" eventually,
-    # not the header
-    defines = _parse_defines(os.path.join(project_description["build_dir"],
-                                          "config", "sdkconfig.h"), sdk_config_path)
-
-    # Add all SOC _caps.h headers to the defines
-    #
-    # kind of a hack, be nicer to add a component info dict in project_description.json
-    soc_path = [p for p in project_description["build_component_paths"] if p.endswith("/soc")][0]
-    soc_headers = glob.glob(os.path.join(soc_path, "soc", project_description["target"],
-                                         "include", "soc", "*_caps.h"))
-    assert len(soc_headers) > 0
-    for soc_header in soc_headers:
-        defines.update(_parse_defines(soc_header, sdk_config_path))
 
     # Call Doxygen to get XML files from the header files
     print("Calling Doxygen to generate latest XML files")

docs/en/conf.py
@@ -18,5 +18,3 @@ copyright = u'2016 - 2020, Espressif Systems (Shanghai) CO., LTD'
 # The language for content autogenerated by Sphinx. Refer to documentation
 # for a list of supported languages.
 language = 'en'
-
-update_exclude_patterns(tags)  # noqa: F405, need to import * from conf_common

docs/idf_extensions/exclude_docs.py
@@ -1,0 +1,12 @@
+# Updates the excluded documents according to the conditional_include_dict {tag:documents}
+def update_exclude_patterns(app, config):
+    for tag, docs in config.conditional_include_dict.items():
+        if not app.tags.has(tag):
+            app.config.exclude_patterns.extend(docs)
+
+
+def setup(app):
+    # Tags are generated together with defines
+    app.connect('config-inited', update_exclude_patterns)
+
+    return {'parallel_read_safe': True, 'parallel_write_safe': True, 'version': '0.1'}

docs/idf_extensions/gen_defines.py
@@ -1,0 +1,72 @@
+# Sphinx extension to integrate defines into the Sphinx Build
+#
+# Runs after the IDF dummy project has been built
+#
+# Then emits the new 'idf-defines-generated' event which has a dictionary of raw text define values
+# that other extensions can use to generate relevant data.
+
+import glob
+import os
+import subprocess
+import re
+
+
+def generate_defines(app, project_description):
+    sdk_config_path = os.path.join(project_description["build_dir"], "config")
+
+    # Parse kconfig macros to pass into doxygen
+    #
+    # TODO: this should use the set of "config which can't be changed" eventually,
+    # not the header
+    defines = get_defines(os.path.join(project_description["build_dir"],
+                                       "config", "sdkconfig.h"), sdk_config_path)
+
+    # Add all SOC _caps.h headers and kconfig macros to the defines
+    #
+    # kind of a hack, be nicer to add a component info dict in project_description.json
+    soc_path = [p for p in project_description["build_component_paths"] if p.endswith("/soc")][0]
+    soc_headers = glob.glob(os.path.join(soc_path, "soc", project_description["target"],
+                                         "include", "soc", "*_caps.h"))
+    assert len(soc_headers) > 0
+
+    for soc_header in soc_headers:
+        defines.update(get_defines(soc_header, sdk_config_path))
+
+    add_tags(app, defines)
+
+    app.emit('idf-defines-generated', defines)
+
+
+def get_defines(header_path, sdk_config_path):
+    defines = {}
+    # Note: we run C preprocessor here without any -I arguments (except "sdkconfig.h"), so assumption is
+    # that these headers are all self-contained and don't include any other headers
+    # not in the same directory
+    print("Reading macros from %s..." % (header_path))
+    processed_output = subprocess.check_output(["xtensa-esp32-elf-gcc", "-I", sdk_config_path,
+                                                "-dM", "-E", header_path]).decode()
+    for line in processed_output.split("\n"):
+        line = line.strip()
+        m = re.search("#define ([^ ]+) ?(.*)", line)
+        if m and not m.group(1).startswith("_"):
+            defines[m.group(1)] = m.group(2)
+
+    return defines
+
+
+def add_tags(app, defines):
+    # try to parse define values as ints and add to tags
+    for name, value in defines.items():
+        try:
+            define_value = int(value.strip("()"))
+            if define_value > 0:
+                app.tags.add(name)
+        except ValueError:
+            continue
+
+
+def setup(app):
+    app.connect('idf-info', generate_defines)
+    app.add_event('idf-defines-generated')
+
+    return {'parallel_read_safe': True, 'parallel_write_safe': True, 'version': '0.1'}

docs/zh_CN/conf.py
@@ -18,5 +18,3 @@ copyright = u'2016 - 2020 乐鑫信息科技（上海）股份有限公司'
 # The language for content autogenerated by Sphinx. Refer to documentation
 # for a list of supported languages.
 language = 'zh_CN'
-
-update_exclude_patterns(tags)  # noqa: F405, need to import * from conf_common

MaiNhan94/esp_idf_c3

commit 0e6de53dccbae33eeb3912eaeaaccac6ee3c244d

doc: Fix passing of build macros to Doxygen
Regression in fbb54184ef006ae6644d0b52878414ec0b2af652, the dictionary
passed to generate_doxygen function was still the project_description
structure.

docs/conf_common.py
@@ -58,6 +58,10 @@ extensions = ['breathe',
               'extensions.toctree_filter',
               'extensions.list_filter',
 
+              # Note: order is important here, events must
+              # be registered by one extension before they can be
+              # connected to another extension
+
               'idf_extensions.include_build_file',
               'idf_extensions.link_roles',
               'idf_extensions.build_system',
@@ -65,11 +69,11 @@ extensions = ['breathe',
               'idf_extensions.gen_toolchain_links',
               'idf_extensions.gen_version_specific_includes',
               'idf_extensions.kconfig_reference',
+              'idf_extensions.gen_defines',
               'idf_extensions.run_doxygen',
               'idf_extensions.gen_idf_tools_links',
               'idf_extensions.format_idf_target',
               'idf_extensions.latex_builder',
-              'idf_extensions.gen_defines',
               'idf_extensions.exclude_docs',
 
               # from https://github.com/pfalcon/sphinx_selective_exclude

docs/idf_extensions/run_doxygen.py
@@ -21,27 +21,9 @@ ALL_KINDS = [
 
 
 def setup(app):
-    # The idf_build_system extension will emit this event once it
-    app.connect('idf-info', generate_doxygen)
-
-    return {'parallel_read_safe': True, 'parallel_write_safe': True, 'version': '0.1'}
-
-
-def _parse_defines(header_path, sdk_config_path):
-    defines = {}
-    # Note: we run C preprocessor here without any -I arguments (except "sdkconfig.h"), so assumption is
-    # that these headers are all self-contained and don't include any other headers
-    # not in the same directory
-    print("Reading macros from %s..." % (header_path))
-    processed_output = subprocess.check_output(["xtensa-esp32-elf-gcc", "-I", sdk_config_path,
-                                                "-dM", "-E", header_path]).decode()
-    for line in processed_output.split("\n"):
-        line = line.strip()
-        m = re.search("#define ([^ ]+) ?(.*)", line)
-        if m and not m.group(1).startswith("_"):
-            defines[m.group(1)] = m.group(2)
-
-    return defines
+    # The idf_build_system extension will emit this event once it has generated documentation macro definitions
+    app.connect('idf-defines-generated', generate_doxygen)
+    return {'parallel_read_safe': True, 'parallel_write_safe': True, 'version': '0.2'}
 
 
 def generate_doxygen(app, defines):